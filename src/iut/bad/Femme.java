87bfab30678483b0e21b6376108e90be56758c7c
haingolaris <larissahaingonirina@gmail.com>
2024-03-28 12:22:53 +0100

    Version 1.9.0: Création de la méthode main() dans Femme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -11,5 +11,10 @@ public class Femme extends Humain {
         super(nom, prenom, age);
     }
 
-    
+    public static void main(String[] args) {
+    	Humain femme = new Humain("Marie", "Jeanne", 30); // Exemple de création d'un objet femme
+    	Humain homme = new Humain("Robert", "Pilier", 35); // Exemple de création d'un objet homme
+
+    	femme.ami(homme); // Appel de la méthode ami pour ajouter homme comme ami de femme
+    }
 }
