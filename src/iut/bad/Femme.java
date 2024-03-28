79df8894db08fb823e4369ad48914168398ecb22
haingolaris <larissahaingonirina@gmail.com>
2024-03-28 11:53:23 +0100

    Version 1.3.0: Création de la classe Humain et modification de Homme/Femme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -1,39 +1,15 @@
 package iut.bad;
 
-public class Femme {
-	private String nom;
-    private String prenom;
-    private int age;
-    
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public Femme(String nom, String prenom, int age) {
-		super();
-		this.nom = nom;
-		this.prenom = prenom;
-		this.age = age;
-	}
-	public Femme() {
-		 this.nom = "";
-	        this.prenom = "";
-	        this.age = 0;
-	}
-    
+public class Femme extends Humain {
+    // Constructeur par défaut
+    public Femme() {
+        super();
+    }
+
+    // Constructeur avec tous les champs
+    public Femme(String nom, String prenom, int age) {
+        super(nom, prenom, age);
+    }
+
     
 }
--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -1,46 +1,13 @@
 package iut.bad;
-public class Homme {
-    private String nom;
-    private String prenom;
-    private int age;
-
+public class Homme extends Humain {
     // Constructeur par défaut
     public Homme() {
-        this.nom = "";
-        this.prenom = "";
-        this.age = 0;
+        super();
     }
 
     // Constructeur avec tous les champs
     public Homme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-
-    // Getters et setters
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+        super(nom, prenom, age);
     }
 
     
